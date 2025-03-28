3254383777c75fe1bb69917983616fa3eae8e399
Kongpob2003 <147183114+Kongpob2003@users.noreply.github.com>
2025-03-28 07:53:33 +0700

    ..

--- obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Mauiv2.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Mauiv2.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mauiv2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fa19e36729295a8a99db7ff9d0e9e33325c331e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+80653171e3b0a42659f26d8c807517f3bc11ab7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mauiv2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mauiv2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
